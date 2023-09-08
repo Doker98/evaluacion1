a56b1556a6c76a334880566bfc53203647016e48
Josémendez <josemendez9803@outlook.com>
2023-09-08 20:43:54 -0300

    git config user.email "josemendez9803@outlook.com"  git config user.name "Doker98"

---
+++ app/src/main/java/cl/IPVG/evaluacion1/MainActivity4.java
@@ -0,0 +1,14 @@
+package cl.IPVG.evaluacion1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+}
